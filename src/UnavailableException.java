8a0e9106ef0c0977f3d88d25279c68d3f1b0b578
irenemachine <irene.haque@gmail.com>
2012-07-24 22:21:39 -0500

    adding custom exception

---
+++ src/UnavailableException.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: irenehaque
+ * Date: 7/24/12
+ * Time: 5:22 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class UnavailableException extends Exception {
+}
